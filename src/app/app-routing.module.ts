3e12358794a916b53b437f4b2b0b91e1338b30fc
Rocio Sajama <ro.sajama14@gmail.com>
2020-05-21 00:16:37 -0300

    primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+
+
+const routes: Routes = [
+  {path:'index' , component: HomeComponent },
+  {path:'registro' , component: RegistroComponent},
+  {path:'galeria' , component: GaleriaComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    RegistroComponent,
+    GaleriaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
